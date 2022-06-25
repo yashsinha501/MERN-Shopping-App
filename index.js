396da4cd67a4e73c605f46881184756de410cee8
yashsinha501 <72601810+yashsinha501@users.noreply.github.com>
2022-06-25 16:38:14 +0530

    folder structure

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
+require('./config/config');
+require('./models/DB');
 const express = require('express');
-const products=require('./assets/products.json');
-const app = express()
-
-
-app.get('/',(req, res) => {
-  res.send('Hello World')
-});
-
-app.get('/getProducts/',(req, res) => {
-
-  let cat=[];
-    let data=products.filter((curData)=>{
-
-      if(curData.category==="jewelery"){
-          cat.push(curData);
-      }    
-    
-    })
-    res.send(cat);
-  });
-
-app.listen(3000);
+const app = express();
+app.use(express.json());
+app.listen(process.env.PORT);
